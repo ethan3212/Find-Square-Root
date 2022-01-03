4cdf2d24b636f0dd45c35b28b2aaafcf8acbd772
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 08:39:44 -0600

    Add files via upload

---
+++ FindSquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace FindSquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number, sqrt;
+            try
+            {
+                Write("Enter number: ");
+                if (!double.TryParse(ReadLine(), out number))
+                    WriteLine("Entered number is not a double: ");
+                if (number < 0)
+                    throw (new ApplicationException("Number can't be negative: "));
+                sqrt = Math.Sqrt(number);
+            }
+            catch (ApplicationException e)
+            {
+                WriteLine("Error: {0}", e.Message);
+                sqrt = 0;
+            }
+            WriteLine("Square root is {0}", sqrt);
+        }
+    }
+}
